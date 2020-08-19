0a32c84b2f9ddb9ac20abc5a4d3f5ff8c4e2ce18
chimdia <fortunechimdiya@gmail.com>
2020-08-19 16:09:51 +0100

    added Navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./navbar.css"
-
+import React from 'react';
+import "./navbar.scss";
+import Logo from '../../assets/avatar.jpg';
 const Navbar = () => {
     return ( 
         <div className="navbar">
             <p className="logo">TransMonitor</p>
             <div className='search'>
-                
+                <i className='fas fa-search icon'></i>
+                <span className='search-text'>Search...</span>
+            </div>
+            <div className="nav-links">
+                <p className="link">Support</p>
+                <p className="link">FAQ</p>
+                <i className="link far fa-bell"></i>
+                <div className="greeting">
+                    <p>Hello</p>
+                    <p>Oluwaleke Ojo</p>
+                </div>
+                <img src={Logo} alt="avatar" className="avatar"/>
             </div>
         </div>
      );
