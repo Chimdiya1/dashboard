2ce55108b0a07c176120f7983ce68900dc975f46
chimdia <fortunechimdiya@gmail.com>
2020-08-19 15:08:43 +0100

    initial commit

---
+++ src/components/navbar/navbar.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./navbar.css"
+
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <p className="logo">TransMonitor</p>
+            <div className='search'>
+                
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
