5f8efd892c88fc9f96a4dc780a1e5ec7442fc697
chimdia <fortunechimdiya@gmail.com>
2020-08-20 17:56:50 +0100

    modified chart

--- src/components/graph/graph.component.jsx
+++ src/components/graph/graph.component.jsx
@@ -1,56 +1,15 @@
 import React from 'react';
-import Chart from '../Chart/Chart.component';
-
-
-const data = [
-    [
-        {
-            name: 'Jan', uv: 4000, pv: 2400, amt: 2400,
-        },
-        {
-            name: 'Feb', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-            name: 'Mar', uv: 2000, pv: 9800, amt: 2290,
-        },
-        {
-            name: 'Apr', uv: 2780, pv: 3908, amt: 2000,
-        },
-        {
-            name: 'May', uv: 1890, pv: 4800, amt: 2181,
-        },
-        {
-            name: 'Jun', uv: 2390, pv: 3800, amt: 2500,
-        },
-    ],
-    [
-        {
-            name: 'Jul', uv: 400, pv: 2400, amt: 2400,
-        },
-        {
-            name: 'Aug', uv: 3000, pv: 1398, amt: 2210,
-        },
-        {
-            name: 'Sep', uv: 2600, pv: 9800, amt: 2290,
-        },
-        {
-            name: 'Oct', uv: 780, pv: 3908, amt: 2000,
-        },
-        {
-            name: 'Nov', uv: 190, pv: 4800, amt: 2181,
-        },
-        {
-            name: 'Dec', uv: 2990, pv: 3800, amt: 2500,
-        },
-    ],
-];
+import ApexChart from '../Chart/Chart.component';
 
 
 
-const Graph = ({range}) => {
+
+
+
+const Graph = ({ firstHalf ,secondHalf, all }) => {
     return ( 
         <div className='graph'>
-            <Chart data={data[range]}/>
+            <ApexChart firstHalf={firstHalf} secondHalf={secondHalf} all={all} />
         </div>
      );
 }
--- src/components/graphBody/graphBody.component.jsx
+++ src/components/graphBody/graphBody.component.jsx
@@ -1,26 +1,71 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './graphBody.scss'
 import Graph from '../graph/graph.component'
+import ApexCharts from 'apexcharts';
+
 const GraphBody = () => {
-    const [Range, setRange] = useState(0)
+    const firstHalf = {
+         start : new Date('01 Jan 2012').getTime(),
+         stop : new Date('01 Jun 2012').getTime(),
+    }
+    const secondHalf = {
+         start : new Date('01 Jun 2012').getTime(),
+         stop : new Date('31 Dec 2012').getTime(),
+    }
+    const all = {
+         start : new Date('01 Jan 2012').getTime(),
+         stop : new Date('31 Dec 2012').getTime(),
+    }
+    const updateData = (timeline) => {
+
+        switch (timeline) {
+            case 'first-half':
+                ApexCharts.exec(
+                    'area-datetime',
+                    'zoomX',
+                    firstHalf.start,
+                    firstHalf.stop
+                );
+                break;
+            case 'second-half':
+                ApexCharts.exec(
+                    'area-datetime',
+                    'zoomX',
+                    secondHalf.start,
+                    secondHalf.stop
+                );
+                break;
+            case 'all':
+                ApexCharts.exec(
+                    'area-datetime',
+                    'zoomX',
+                    all.start,
+                    all.stop
+                );
+                break;
+            default:
+        }
+    }
+
     return ( 
         <div className="graphBody">
             <div className='graphBody-header'>
                 <h3>Today: 5, Aug 2018</h3>
-                <select onChange={(e)=>{setRange(e.target.value)}} className='date-select'>
-                    <option value={0}>1 Jan - 1 Jun</option>
-                    <option value={1}>1 Jun - 31 Dec</option>
+                <select onChange={(e)=>{updateData(e.target.value)}} className='date-select'>
+                    <option value='first-half'>1 Jan - 1 Jun</option>
+                    <option value='second-half'>1 Jun - 31 Dec</option>
+                    <option value='all'>All time</option>
                 </select>
                 <div className='buttons'>
                     <button onClick={() => {
-                        return Range === 1 ? setRange(0) : setRange(1)
+                        updateData('first-half')
                     }}><i className='fas arrow fa-chevron-left'></i></button>
                     <button onClick={() => {
-                        return Range === 0 ? setRange(1) : setRange(0)
+                        updateData('second-half')
                     }}><i className='fas arrow fa-chevron-right'></i></button>
                 </div>
             </div>
-            <Graph range={Range}/>
+            <Graph range={Range} firstHalf={firstHalf} secondHalf={secondHalf} all={all}/>
         </div>
      );
 }
